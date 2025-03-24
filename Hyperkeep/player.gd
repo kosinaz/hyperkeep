e4d6e67fe0449f725175098504ff23589574860a
Kosina Zoltán <kosinaz@gmail.com>
2025-03-24 20:00:49 +0100

    add player

---
+++ Hyperkeep/player.gd
@@ -0,0 +1,68 @@
+extends CharacterBody3D
+
+@export var speed = 2.0
+@export var jump_velocity = 4.5
+@export var mouse_sensitivity = 0.002
+
+# Camera bobbing
+@export var bob_frequency = 0.0
+@export var bob_amplitude = 0.0
+var bob_time = 0.0
+var initial_camera_y = 0.0
+
+# Get the gravity from the project settings
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@onready var camera = $Camera3D
+@onready var camera_ray = $Camera3D/RayCast3D
+
+func _ready():
+	# Capture the mouse
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	initial_camera_y = camera.position.y
+
+func _input(event):
+	# Mouse look
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		camera.rotate_x(-event.relative.y * mouse_sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+	
+	# Toggle mouse capture with Escape key
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_ESCAPE:
+			if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			else:
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _physics_process(delta):
+	# Add gravity
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Get input direction
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	# Handle movement
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+		
+		# Camera bobbing when moving
+		bob_time += delta * bob_frequency * velocity.length() / speed
+		camera.position.y = initial_camera_y + sin(bob_time) * bob_amplitude
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+		
+		# Gradually return camera to neutral position when not moving
+		bob_time = 0
+		camera.position.y = lerp(camera.position.y, initial_camera_y, delta * 5.0)
+
+	move_and_slide()
--- Hyperkeep/wall.gd
+++ Hyperkeep/wall.gd
@@ -1,5 +1,6 @@
-extends MeshInstance3D
+extends Node3D
 
+# Export variables for tiling control
 @export var wall_tiling: float = 3.0
 @export var floor_tiling: float = 3.0
 @export var ceiling_tiling: float = 3.0
@@ -8,9 +9,31 @@ extends MeshInstance3D
 # Reference to the material resource
 @export var material_resource: ShaderMaterial
 
+var mesh_instance: MeshInstance3D
+
 func _ready():
-	# If no material is assigned, create a new one
+	# Create a cube mesh
+	var cube_mesh = BoxMesh.new()
+	cube_mesh.size = Vector3(1, 1, 1)
+	
+	# Create a MeshInstance3D node
+	mesh_instance = MeshInstance3D.new()
+	mesh_instance.mesh = cube_mesh
+	add_child(mesh_instance)
+	
+	# Add collision
+	var static_body = StaticBody3D.new()
+	add_child(static_body)
+	
+	var collision_shape = CollisionShape3D.new()
+	var box_shape = BoxShape3D.new()
+	box_shape.size = Vector3(1, 1, 1)
+	collision_shape.shape = box_shape
+	static_body.add_child(collision_shape)
+	
+	# Set up the material
 	if material_resource == null:
+		# Create a ShaderMaterial
 		material_resource = ShaderMaterial.new()
 		material_resource.shader = load("res://wall_shader.gdshader")
 		
@@ -18,35 +41,59 @@ func _ready():
 		_load_textures()
 	
 	# Apply the material to the mesh
-	material_override = material_resource
+	mesh_instance.material_override = material_resource
 	
 	# Set the tiling parameters
 	_update_material_parameters()
 
 func _load_textures():
-	# Wall textures
-	material_resource.set_shader_parameter("wall_diffuse", load("res://textures/wall_1/wall_diffuse.bmp"))
-	material_resource.set_shader_parameter("wall_edge", load("res://textures/wall_1/wall_edge.bmp"))
-	material_resource.set_shader_parameter("wall_height", load("res://textures/wall_1/wall_height.bmp"))
-	material_resource.set_shader_parameter("wall_metallic", load("res://textures/wall_1/wall_metallic.bmp"))
-	material_resource.set_shader_parameter("wall_normal", load("res://textures/wall_1/wall_normal.bmp"))
-	material_resource.set_shader_parameter("wall_smoothness", load("res://textures/wall_1/wall_smoothness.bmp"))
+	# Load wall textures
+	var wall_diffuse = load("res://textures/wall_1/wall_diffuse.bmp")
+	var wall_edge = load("res://textures/wall_1/wall_edge.bmp")
+	var wall_height = load("res://textures/wall_1/wall_height.bmp")
+	var wall_metallic = load("res://textures/wall_1/wall_metallic.bmp")
+	var wall_normal = load("res://textures/wall_1/wall_normal.bmp")
+	var wall_smoothness = load("res://textures/wall_1/wall_smoothness.bmp")
 	
-	# Floor textures
-	material_resource.set_shader_parameter("floor_diffuse", load("res://textures/floor_1/floor_diffuse.bmp"))
-	material_resource.set_shader_parameter("floor_edge", load("res://textures/floor_1/floor_edge.bmp"))
-	material_resource.set_shader_parameter("floor_height", load("res://textures/floor_1/floor_height.bmp"))
-	material_resource.set_shader_parameter("floor_metallic", load("res://textures/floor_1/floor_metallic.bmp"))
-	material_resource.set_shader_parameter("floor_normal", load("res://textures/floor_1/floor_normal.bmp"))
-	material_resource.set_shader_parameter("floor_smoothness", load("res://textures/floor_1/floor_smoothness.bmp"))
+	# Load floor textures
+	var floor_diffuse = load("res://textures/floor_1/floor_diffuse.bmp")
+	var floor_edge = load("res://textures/floor_1/floor_edge.bmp")
+	var floor_height = load("res://textures/floor_1/floor_height.bmp")
+	var floor_metallic = load("res://textures/floor_1/floor_metallic.bmp")
+	var floor_normal = load("res://textures/floor_1/floor_normal.bmp")
+	var floor_smoothness = load("res://textures/floor_1/floor_smoothness.bmp")
 	
-	# Ceiling textures
-	material_resource.set_shader_parameter("ceiling_diffuse", load("res://textures/ceiling_1/ceiling_diffuse.bmp"))
-	material_resource.set_shader_parameter("ceiling_edge", load("res://textures/ceiling_1/ceiling_edge.bmp"))
-	material_resource.set_shader_parameter("ceiling_height", load("res://textures/ceiling_1/ceiling_height.bmp"))
-	material_resource.set_shader_parameter("ceiling_metallic", load("res://textures/ceiling_1/ceiling_metallic.bmp"))
-	material_resource.set_shader_parameter("ceiling_normal", load("res://textures/ceiling_1/ceiling_normal.bmp"))
-	material_resource.set_shader_parameter("ceiling_smoothness", load("res://textures/ceiling_1/ceiling_smoothness.bmp"))
+	# Load ceiling textures
+	var ceiling_diffuse = load("res://textures/ceiling_1/ceiling_diffuse.bmp")
+	var ceiling_edge = load("res://textures/ceiling_1/ceiling_edge.bmp")
+	var ceiling_height = load("res://textures/ceiling_1/ceiling_height.bmp")
+	var ceiling_metallic = load("res://textures/ceiling_1/ceiling_metallic.bmp")
+	var ceiling_normal = load("res://textures/ceiling_1/ceiling_normal.bmp")
+	var ceiling_smoothness = load("res://textures/ceiling_1/ceiling_smoothness.bmp")
+	
+	# Set shader parameters for wall textures
+	material_resource.set_shader_parameter("wall_diffuse", wall_diffuse)
+	material_resource.set_shader_parameter("wall_edge", wall_edge)
+	material_resource.set_shader_parameter("wall_height", wall_height)
+	material_resource.set_shader_parameter("wall_metallic", wall_metallic)
+	material_resource.set_shader_parameter("wall_normal", wall_normal)
+	material_resource.set_shader_parameter("wall_smoothness", wall_smoothness)
+	
+	# Set shader parameters for floor textures
+	material_resource.set_shader_parameter("floor_diffuse", floor_diffuse)
+	material_resource.set_shader_parameter("floor_edge", floor_edge)
+	material_resource.set_shader_parameter("floor_height", floor_height)
+	material_resource.set_shader_parameter("floor_metallic", floor_metallic)
+	material_resource.set_shader_parameter("floor_normal", floor_normal)
+	material_resource.set_shader_parameter("floor_smoothness", floor_smoothness)
+	
+	# Set shader parameters for ceiling textures
+	material_resource.set_shader_parameter("ceiling_diffuse", ceiling_diffuse)
+	material_resource.set_shader_parameter("ceiling_edge", ceiling_edge)
+	material_resource.set_shader_parameter("ceiling_height", ceiling_height)
+	material_resource.set_shader_parameter("ceiling_metallic", ceiling_metallic)
+	material_resource.set_shader_parameter("ceiling_normal", ceiling_normal)
+	material_resource.set_shader_parameter("ceiling_smoothness", ceiling_smoothness)
 
 func _update_material_parameters():
 	# Set the tiling parameters
@@ -54,10 +101,3 @@ func _update_material_parameters():
 	material_resource.set_shader_parameter("floor_tiling", floor_tiling)
 	material_resource.set_shader_parameter("ceiling_tiling", ceiling_tiling)
 	material_resource.set_shader_parameter("height_scale", height_scale)
-
-# This function can be called to update the tiling at runtime if needed
-func set_tiling(wall: float, floor_tile: float, ceiling: float):
-	wall_tiling = wall
-	floor_tiling = floor_tile
-	ceiling_tiling = ceiling
-	_update_material_parameters()
